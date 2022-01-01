2a0653f6e072b2a6e66dc93b9816baf2faa92ac6
shareef <nadeemshareef934@gmail.com>
2022-01-01 16:58:47 +0530

    ✨ enables commanderjs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
-// We are writing the above to make sure that this file is run as a script
-// without specifying the node command.
+
+import { program } from "commander";
+
+program.version("0.0.1");
 
 import { MongoClient } from "mongodb";
 
@@ -9,77 +11,86 @@ const uri =
 
 const [, , ...data] = process.argv;
 
-function main() {
-    if (data.length === 0) {
-        console.log(`Welcome to CLI-Todo for linux lovers!`);
-        return;
-    }
+if (data.length === 0) {
+    console.log(`Welcome to CLI-Todo for linux lovers!`);
+}
 
-    if (data) {
-        const [command, ...args] = data;
-        if (command === "help") {
-            console.log(`Welcome to the help section of CLI-Todo for linux lovers!
+if (data) {
+    const [command, ...args] = data;
+    if (command === "help") {
+        console.log(`Welcome to the help section of CLI-Todo for linux lovers!
 Commands:
     todo add: Adds a task to the list
     todo ls: Lists all the tasks
     todo del: Deletes a task from the list
     todo help: Displays the help menu`);
-            return;
+    }
+
+    let client;
+
+    async function connectToDb() {
+        try {
+            client = await MongoClient.connect(uri);
+        } catch (err) {
+            console.log(`Couldn't connect to database`, err);
         }
+    }
 
-        let client;
-
-        async function connectToDb() {
-            try {
-                client = await MongoClient.connect(uri);
-            } catch (err) {
-                console.log(`Couldn't connect to database`, err);
-            }
-        }
-
-        if (command === "ls") {
-            connectToDb().then(() => {
-                const db = client.db("cli-todo");
-                db.collection("todos")
-                    .find({})
-                    .toArray()
-                    .then((docs) => {
-                        const todos = docs;
-                        let pendingTodos = todos.filter(
-                            (todo) => !todo.completed
-                        );
-                        let completedTodos = todos.filter(
-                            (todo) => todo.completed
-                        );
-                        console.log("List of your tasks");
-                        console.log(`pending tasks ${pendingTodos.length} `);
-                        pendingTodos.forEach((todo, index) => {
-                            console.log(`${index + 1}. ${todo.todo}`);
-                        });
-                        console.log(`completed tasks ${completedTodos.length}`);
-                        completedTodos.forEach((todo, index) => {
-                            console.log(`${index + 1}. ${todo.todo}`);
-                        });
-                        client.close();
+    if (command === "ls") {
+        connectToDb().then(() => {
+            const db = client.db("cli-todo");
+            db.collection("todos")
+                .find({})
+                .toArray()
+                .then((docs) => {
+                    const todos = docs;
+                    let pendingTodos = todos.filter((todo) => !todo.completed);
+                    let completedTodos = todos.filter((todo) => todo.completed);
+                    console.log("List of your tasks");
+                    console.log(`pending tasks ${pendingTodos.length} `);
+                    pendingTodos.forEach((todo, index) => {
+                        console.log(`${index + 1}. ${todo.todo}`);
                     });
-            });
-            return;
-        }
-
-        if (command === "add") {
-            connectToDb().then(() => {
-                const [todo] = args;
-                const db = client.db("cli-todo");
-                db.collection("todos")
-                    .insertOne({ todo, completed: false })
-                    .then(() => {
-                        console.log(`Added ${todo} to your list of tasks`);
-                        client.close();
+                    console.log(`completed tasks ${completedTodos.length}`);
+                    completedTodos.forEach((todo, index) => {
+                        console.log(`${index + 1}. ${todo.todo}`);
                     });
-            });
-            return;
-        }
+                    client.close();
+                });
+        });
+    }
+
+    if (command === "add") {
+        connectToDb().then(() => {
+            const [todo] = args;
+            const db = client.db("cli-todo");
+            db.collection("todos")
+                .insertOne({ todo, completed: false })
+                .then(() => {
+                    console.log(`Added ${todo} to your list of tasks`);
+                    client.close();
+                });
+        });
+    }
+
+    if (command === "del") {
+        connectToDb().then(() => {
+            const [index] = args;
+            const db = client.db("cli-todo");
+            db.collection("todos")
+                .findOneAndDelete({ _id: index })
+                .then((result) => {
+                    if (result.value) {
+                        console.log(
+                            `Deleted ${result.value.todo} from your list of tasks`
+                        );
+                    } else {
+                        console.log(`Couldn't find task with id ${index}`);
+                    }
+                    client.close();
+                });
+        });
     }
 }
 
-main();
+program.parse(process.argv);
